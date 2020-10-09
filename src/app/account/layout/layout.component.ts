4cd963ff2b6d8870b73125fc6b767173a41551a8
kwan2881 <k_sophakul@hotmail.com>
2020-10-09 19:22:33 +0700

    register and login

--- src/app/account/layout/layout.component.ts
+++ src/app/account/layout/layout.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountService } from 'src/app/_service/account.service';
 
-@Component({
-  selector: 'app-layout',
-  templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
-})
-export class LayoutComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+@Component({ templateUrl: 'layout.component.html' })
+export class LayoutComponent {
+    constructor(
+        private router: Router,
+        private accountService: AccountService
+    ) {
+        // redirect to home if already logged in
+        if (this.accountService.userValue) {
+            this.router.navigate(['/']);
+        }
+    }
 }
